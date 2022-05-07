83a9fd194b435dcc1affa468214f207ef4e96264
lucasboot <varela.alves@outlook.com>
2022-05-07 16:27:57 -0300

    faltam os testes e organizar o código

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ arr.push(ridgewood);
 //Function to visualize the cost of each hotel
 function printCosts (hotels){
     console.log('Lakewood: ' + hotels[0].cost);
-    console.log('\nBridgewood: ' + hotels[1].cost);
-    console.log('\nRidgewood: ' + hotels[2].cost);
+    console.log('Bridgewood: ' + hotels[1].cost);
+    console.log('Ridgewood: ' + hotels[2].cost);
 
 }
 //Function to determinate the indexes of all occurrences of the lowest cost
@@ -39,7 +39,7 @@ function getCheapestHotel (input) { //DO NOT change the function's name.
     const myArray = text.split(" ");
     weekEndDays += (text.match(/sun/g) || []).length;
     weekEndDays += (text.match(/sat/g) || []).length;
-    weekDays = myArray.length - 1 - weekEndDays;
+    weekDays = myArray.length - 1- weekEndDays; //-1 refers to not counting the 'regular'/'reward' info as a day
     const hotels = [];
     if(myArray[0] === 'Regular'){
         let hotel1 = {
@@ -77,10 +77,14 @@ function getCheapestHotel (input) { //DO NOT change the function's name.
         hotels.push(hotel2);
         hotels.push(hotel3);
         indexes = minCost(hotels);
+    } else {
+        console.log('Entrada inválida');
+        return null;
     }
+    printCosts(hotels);
     if(indexes.length === 1){ //No multiple occurence of lowest cost
         return hotels[indexes[0]].name;
-    } else {
+    } else if(indexes.length > 1) {
         let best =indexes[0]; 
         let value=arr[indexes[best]].rate;
         for (let index = 1; index < indexes.length; index++) {
@@ -90,14 +94,16 @@ function getCheapestHotel (input) { //DO NOT change the function's name.
             }
           }
           return hotels[best].name;
+    } else {
+        console.log('Erro na execução...')
+        return null; 
     }
-    return null;
 }
 
 console.log(getCheapestHotel('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)'));
 console.log('------------------------------------------------------------');
 console.log(getCheapestHotel('Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)'));
 console.log('------------------------------------------------------------');
-console.log(getCheapestHotel('Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)'));
+console.log(getCheapestHotel('Reward: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)'));
 
 exports.getCheapestHotel = getCheapestHotel
